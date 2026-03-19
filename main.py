f86a23171100b594dc83587fee1ac92dd6daace1
jay7491 <jay7491@naver.com>
2026-03-19 16:57:50 +0900

    엑셀파일로 저장 기능 추가

--- main.py
+++ main.py
@@ -5,11 +5,12 @@
 3. 마지막 요일 휴일
 4. 한국 음력 휴일
 5. 한국 대체공휴일
+6. 한국 임시공휴일
 '''
 
 from parser import parse_input
 from holiday_service import get_holidays_by_query
-from printer import print_holidays
+from printer import print_holidays, print_to_csv
 
 
 def main() -> None:
@@ -25,7 +26,12 @@ def main() -> None:
             print_holidays(holidays)
         except ValueError as e:
             print(f"입력 오류: {e}")
-        print()
+        
+        save_or_not = input("엑셀로 저장하시겠습니까? y/n: ")
+        if save_or_not.lower() == "y":
+            print_to_csv(holidays, query)
+        else:
+            print()
 
 if __name__ == '__main__':
     main()
--- printer.py
+++ printer.py
@@ -1,4 +1,6 @@
-from models import Holiday
+from models import Holiday, Query
+import pandas as pd
+import os
 
 
 def print_holidays(holidays: list[Holiday]) -> None:
@@ -12,3 +14,44 @@ def print_holidays(holidays: list[Holiday]) -> None:
             print(f"[{holiday.country}] {holiday.day} {holiday.name} ({holiday.note})")
         else:
             print(f"[{holiday.country}] {holiday.day} {holiday.name}")
+
+def print_to_csv(holidays: list[Holiday], query: Query) -> None:
+    if len(holidays) == 0:
+        print("해당 조건의 휴일이 없습니다.")
+        return
+    
+    country = []
+    day = []
+    name = []
+    note = []
+    for holiday in holidays:
+        country.append(holiday.country)
+        day.append(holiday.day)
+        name.append(holiday.name)
+        note.append(holiday.note)
+    
+    holidays_df = pd.DataFrame({"국가":country, "날짜": day, "휴일명": name, "비고": note})
+
+    base_dir = os.getcwd()
+    folder_path = os.path.join(base_dir, '휴일리스트')
+    os.makedirs(folder_path, exist_ok=True)
+
+    try:
+        if query.target is None:
+            if query.month is None:
+                filename = f"{query.year}.xlsx"
+            else:
+                filename = f"{query.year}_{query.month}.xlsx"
+        else:
+            if query.month is None:
+                filename = f"{query.target}_{query.year}.xlsx"
+            else:
+                filename = f"{query.target}_{query.year}_{query.month}.xlsx"
+
+        file_path = os.path.join(folder_path, filename)
+        holidays_df.to_excel(file_path, index=False, engine='openpyxl')
+    except:
+        print("에러 발생, 저장 불가\n")
+    else:
+        print(filename + " 저장 완료\n")
+
